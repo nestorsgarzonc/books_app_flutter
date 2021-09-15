b8bfa339cb787e6e31d814d7865579c8c25f44e7
Nestor Sebastian Garzon Contreras <negarzonc@unal.edu.co>
2021-09-14 19:46:14 -0500

    Added onGenerateRoute

--- lib/core/navigator/navigator.dart
+++ lib/core/navigator/navigator.dart
@@ -1,3 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_library/main.dart';
+
 class CustomNavigator {
-  
+  static Route onGenerateRoute(RouteSettings routeSettings) {
+    switch (routeSettings.name) {
+      case HomeScreen.route:
+        return MaterialPageRoute(builder: (context) => const HomeScreen());
+      default:
+        return MaterialPageRoute(builder: (context) => const HomeScreen());
+    }
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_library/core/navigator/navigator.dart';
 import 'package:flutter_library/core/scroll/custom_scroll_behavior.dart';
 import 'package:flutter_library/core/utils/date_utils.dart';
 
@@ -25,13 +26,15 @@ class MyApp extends StatelessWidget {
           filled: true,
         ),
       ),
-      home: const HomeScreen(),
+      onGenerateRoute: CustomNavigator.onGenerateRoute,
     );
   }
 }
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
+
+  static const route = '/';
 
   @override
   Widget build(BuildContext context) {
