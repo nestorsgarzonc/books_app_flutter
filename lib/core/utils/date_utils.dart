155eda56a6be0c2f80ddf812e1b234ce4e9db46f
Nestor Sebastian Garzon Contreras <negarzonc@unal.edu.co>
2021-09-14 19:25:30 -0500

    Initial commit

---
+++ lib/core/utils/date_utils.dart
@@ -0,0 +1,12 @@
+class CustomDateUtils {
+  static String getGreeting() {
+    final hour = DateTime.now().hour;
+    if (hour < 12) {
+      return 'Good Morning';
+    } else if (hour < 18) {
+      return 'Good Afternoon';
+    } else {
+      return 'Good Evening';
+    }
+  }
+}
---
+++ lib/core/utils/theme/theme.dart
@@ -0,0 +1,6 @@
+import 'dart:ui';
+
+class CustomTheme {
+  static const primaryColor = Color(0xFF3F51B5);
+  static const secundaryColor = Color(0xFF695310);
+}
